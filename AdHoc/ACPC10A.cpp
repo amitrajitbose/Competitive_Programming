43f58e6c634e680ce8b2e3c828e707047420787e
amitrajitbose <amitrajitbose9@gmail.com>
2018-06-07 19:38:08 +0530

    Add SPOJ code

---
+++ AdHoc/ACPC10A.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main(){
+	int a,b,c;
+	cin>>a>>b>>c;
+	while(a!=0 || b!=0 || c!=0){
+		if(b-a==c-b){
+			//Arithmetic Progression
+			cout<<"AP "<<(c+(b-a))<<endl;
+		}
+		else if(a*c==b*b){
+			//Geometric Progression
+			cout<<"GP "<<(c*(b/a))<<endl;
+		}
+		cin>>a>>b>>c;
+	}
+	return 0;
+}
