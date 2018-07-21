59a0118f6c2c0f4f85cc82eb919498911aa104b5
amitrajitbose <amitrajitbose9@gmail.com>
2018-07-21 14:04:49 +0530

    Trying with quicksort

--- Divide_Conquer/QuickSort.c
+++ Divide_Conquer/QuickSort.c
@@ -1,3 +1,11 @@
 /*
 Implementation of QuickSort algorithm
 */
+#include <stdio.h>
+#include <stdlib.h>
+void swap(int *a,int *b){
+	int t=*a;
+	*a=*b;
+	*b=t;
+}
+int Partition(int A[],int p, int r)
