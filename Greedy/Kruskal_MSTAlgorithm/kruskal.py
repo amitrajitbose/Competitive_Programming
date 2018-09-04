9ec10a5f9d1325de1a8f2c1f08c87709db752704
amitrajitbose <amitrajitbose9@gmail.com>
2018-09-05 01:26:12 +0530

    Resolved Issue #1

--- Greedy/Kruskal_MSTAlgorithm/kruskal.py
+++ Greedy/Kruskal_MSTAlgorithm/kruskal.py
@@ -1,36 +1,45 @@
 '''
-Kruskal's Algorithm Approach
+Kruskal's Algorithm for Minimum Spanning Tree
 Author: Amitrajit Bose
 Approach : Greedy
 Aim: To calculate minimum cost spanning tree
 '''
 
-def findSet(vertex, A):
-	if(vertex in A):
-		return True
-	else:
-		return False
+def findSet(vertex, connected):
+	return connected[vertex]
+
+def union(connected,v,v1,v2):
+	for i in range(v+1):
+		if(connected[i]==v2):
+			connected[i]=v1
+	return connected
+
 def kruskal(graph, v):
 	#graph is a set of tuples of edges
 	#define the null set A that will contain marked/visited vertices
 	mstEdgeSet=[]
 	mstVertexSet=[]
 	totalWeight=0
+	connected=[]
+	#initialising connected
+	for i in range(v+1):
+		connected.append(i)
 
 	#sort the tuples in the graph w.r.t their edges
 	graph=sorted(graph, key=lambda x: x[2])
-	#print(graph)
 	totalEdges = v-1
-	mstVertexSet.append(graph[0][0]) #adding the first node by default
 	for e in graph:
 		if(totalEdges==0):
 			break
-		if((findSet(e[0],mstVertexSet) != findSet(e[1],mstVertexSet))or(findSet(e[0],mstVertexSet)==False and findSet(e[1],mstVertexSet)==False)):
+		v1=findSet(e[0],connected)
+		v2=findSet(e[1],connected)
+		if(v1 != v2):
 			#so we append the edge to mst edge set
 			mstEdgeSet.append(e)
 			totalWeight += e[2]
 			totalEdges -= 1
 			mstVertexSet.append(e[1])
+			connected = union(connected, v, v1, v2)
 	return (totalWeight, mstEdgeSet)
 
 def main():
