75e86444f0cd94d3d3323f6bebd93727aed8a788
amitrajitbose <amitrajitbose9@gmail.com>
2018-11-20 01:19:05 +0530

    explanations for nQueens

--- CS591/Backtracking/nqueen.c
+++ CS591/Backtracking/nqueen.c
@@ -5,19 +5,23 @@ int x[N];
 
 int place(int k,int i)
 {
+	// this function checks if placing a queen at i is possible
 	int j;
 	for(j=0;j<k;j++)
 	{
+		//checks if any other queen previously is present is the same column
+		//then also check if it is present on the diagonal
 		if(x[j]==i || abs(x[j]-i)==abs(j-k) )
 			return 0;
 	}
 	return 1;
 }
-int k=1;
+
 void printer(int x[],int n)
 {
 	int i;
-  printf("Sol-%d  ",k++);
+  	static int kc=1;
+  	printf("Sol-%d  ",k++);
 	for(i=0;i<n;i++)
 	{
 		printf("%d ",x[i]);
@@ -31,7 +35,11 @@ void nqueen(int k,int n)
 	{
 		if(place(k,i))
 		{
+			//if placeable then place
 			x[k]=i;
+			//check if any more k is possible or not, if not then print that combination
+			//k is the number if queens placed
+			//if more queens can be placed then recurse with k=k+1 
 			if(k==n-1)
 				printer(x,N);
 			else
