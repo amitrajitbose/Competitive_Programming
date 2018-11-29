21c6cbe6c1f10232e0d00a001bdaf0684d1a43f6
Amitrajit Bose <amitrajitbose9@gmail.com>
2018-11-29 11:02:13 +0530

    DCP#10

--- AdHoc/task_scheduler.py
+++ AdHoc/task_scheduler.py
@@ -1,7 +1,11 @@
 '''
 DCP #10
 
+This problem was asked by Apple.
+
+Implement a job scheduler which takes in a function f and an integer n, and calls f after n milliseconds.
 '''
+
 import time
 def func1():
 	print("HELLO")
