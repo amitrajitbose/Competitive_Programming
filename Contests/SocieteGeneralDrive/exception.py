1a7c0cd5790913d84889130c55607b6012304e7a
Amitrajit Bose <amitrajitbose9@gmail.com>
2019-10-15 00:34:28 +0530

    Removed hiring contest questions, sorry.. it is not legal

--- Contests/SocieteGeneralDrive/exception.py
+++
@@ -1,7 +0,0 @@
-class GradeTooHighException(Exception):
-	def __init__(self):
-		Exception.__init__(self, "Grade Too High")
-
-class GradeTooLowException(Exception):
-	def __init__(self):
-		Exception.__init__(self, "Grade Too Low")
