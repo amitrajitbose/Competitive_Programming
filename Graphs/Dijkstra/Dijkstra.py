196b2e1287c83e9677cd3dd5c95412cde7b0ee82
Amitrajit_Bose@Mac <ar-amitrajit.bose@rakuten.com>
2020-05-19 12:36:11 +0530

    Reorganisation of code

--- Graphs/Dijkstra/Dijkstra.py
+++ Graphs/Dijkstra/Dijkstra.py
@@ -37,3 +37,15 @@ class Solution:
     def solve(self, A, B, C):
         graph = getGraph(A,B)
         return [i[1] if i[1]!=float('inf') else -1 for i in calculate_distances(graph, C)]
+
+'''
+#EXAMPLE GRAPH AFTER PROCESSING THROUGH getGraph(numberOfVertices, inputMatrix)
+example_graph = {
+    'U': {'V': 2, 'W': 5, 'X': 1},
+    'V': {'U': 2, 'X': 2, 'W': 3},
+    'W': {'V': 3, 'U': 5, 'X': 3, 'Y': 1, 'Z': 5},
+    'X': {'U': 1, 'V': 2, 'W': 3, 'Y': 1},
+    'Y': {'X': 1, 'W': 1, 'Z': 1},
+    'Z': {'W': 5, 'Y': 1},
+}
+'''
