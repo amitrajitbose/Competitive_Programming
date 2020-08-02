e2d466aa7fa3aade10457de9009ff3643b4e628a
Amitrajit Bose <amitrajitbose9@gmail.com>
2020-08-02 22:16:12 +0530

    Create BSTIterator.py

---
+++ Tree/BSTIterator.py
@@ -0,0 +1,42 @@
+# LC173
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class BSTIterator:
+
+    def __init__(self, root: TreeNode):
+        if not root:
+            False
+        self.stack = []
+        self.dfsLeft(root)
+    
+    def dfsLeft(self, node):
+        while node:
+            self.stack.append(node)
+            node = node.left
+
+    def next(self) -> int:
+        """
+        @return the next smallest number
+        """
+        nextElem = self.stack.pop(-1)
+        if nextElem.right:
+            self.dfsLeft(nextElem.right)
+        return nextElem.val
+        
+
+    def hasNext(self) -> bool:
+        """
+        @return whether we have a next smallest number
+        """
+        return len(self.stack) != 0
+
+
+# Your BSTIterator object will be instantiated and called as such:
+# obj = BSTIterator(root)
+# param_1 = obj.next()
+# param_2 = obj.hasNext()
