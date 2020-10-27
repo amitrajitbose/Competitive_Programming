61f458e0e23c5d1f0eafadc9cb4b626485fa73de
PK-100 <68665655+PK-100@users.noreply.github.com>
2020-10-27 18:43:42 +0530

    Create Squares_of_a_Sorted_Array.py

---
+++ TwoPointerSW/Squares_of_a_Sorted_Array.py
@@ -0,0 +1,19 @@
+# Given an array of integers A sorted in non-decreasing order, return an array of the squares of each number, also in sorted non-decreasing order.
+
+# Two Pointer Solution
+class Solution:
+    def sortedSquares(self, A: List[int]) -> List[int]: # TIme Complexity = O(n) Space Complexity = O(n) 
+        l = len(A)
+        ans = [None]*l
+        i = 0
+        j = l-1
+        for k in range(l-1,-1,-1):
+            if abs(A[i]) > abs(A[j]):
+                ans[k]=A[i]**2
+                i+=1
+            else:
+                ans[k]=A[j]**2
+                j-=1
+        return ans
+        
+        
