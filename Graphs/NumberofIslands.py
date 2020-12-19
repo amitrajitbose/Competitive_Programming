945a67f527953fbf27aa21f1e701841b6533660d
Amitrajit Bose <amitrajitbose9@gmail.com>
2020-12-19 21:33:23 +0530

    Update NumberofIslands.py

--- Graphs/NumberofIslands.py
+++ Graphs/NumberofIslands.py
@@ -1,4 +1,5 @@
 # LC 200
+# DFS
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
         
@@ -26,4 +27,34 @@ class Solution:
                     if grid[i][j] == '1':
                         numIslands += dfs(grid, i, j)
             return numIslands
+
+#BFS
+from collections import deque
+class Solution:
+    def isvalid(self, mat, i, j):
+        if i<0 or i>=len(mat) or j<0 or j>=len(mat[i]):
+            return False
+        if mat[i][j] != '1':
+            return False
+        return True
+    
+    def numIslands(self, binaryMatrix: List[List[str]]) -> int:
+        if not binaryMatrix or len(binaryMatrix) <= 0:
+            return 0
+        res = 0
+        queue = deque()
+        for i in range(len(binaryMatrix)):
+            for j in range(len(binaryMatrix[i])):
+                if binaryMatrix[i][j] == '1':
+                    res += 1
+                    queue.append([i, j])
+                    while queue:
+                        nexti, nextj = queue.popleft()
+                        if self.isvalid(binaryMatrix, nexti, nextj):
+                            binaryMatrix[nexti][nextj] = '2' # mark
+                            queue.append([nexti, nextj+1])
+                            queue.append([nexti+1, nextj])
+                            queue.append([nexti, nextj-1])
+                            queue.append([nexti-1, nextj])
+        return res
 
